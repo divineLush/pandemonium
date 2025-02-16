11ea0df6f683e1d143e969614d3b11269d0cb510
divineLush <alvipath@yahoo.com>
2025-02-16 18:18:12 +0700

    even more elements

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const color = '#ff8eff'
+const rotation = 90 * (Math.PI / 180)
 
 const torus = (...args) => {
   const geometry = new THREE.TorusGeometry(...args)
@@ -43,42 +44,84 @@ tor3.position.set(42, 18.5, 0)
 const tor4 = torus(3, 1, 30, 100)
 tor4.position.set(12, 18.5, 0)
 
-// VERTICAL CYLINDERS
-const cylinder0 = cylinder(1, 1, 104, 40)
-cylinder0.rotateZ(90 * (Math.PI / 180))
+const tor5 = torus(3, 1, 30, 100)
+tor5.position.set(67.5, -27.5, 0)
 
-const cylinder1 = cylinder(1, 1, 45, 40)
+const tor6 = torus(3, 1, 30, 100)
+tor6.position.set(66.5, 27.5, 0)
+
+// VERTICAL CYLINDERS
+const cylinder0 = cylinder(1, 1, 104, 60)
+cylinder0.rotateZ(rotation)
+
+const cylinder1 = cylinder(1, 1, 45, 60)
 cylinder1.position.set(-48, -7.5, 0)
 
-const cylinder2 = cylinder(1, 1, 75, 40)
+const cylinder2 = cylinder(1, 1, 75, 60)
 cylinder2.position.set(48, -7.5, 0)
 
-const cylinder3 = cylinder(1, 1, 60, 40)
+const cylinder3 = cylinder(1, 1, 60, 60)
 cylinder3.position.set(36, 0, 0)
 
-const cylinder4 = cylinder(1, 1, 60, 40)
+const cylinder4 = cylinder(1, 1, 60, 60)
 cylinder4.position.set(20, 0, 0)
 
-const cylinder5 = cylinder(1, 1, 60, 40)
+const cylinder5 = cylinder(1, 1, 60, 60)
 cylinder5.position.set(4, 0, 0)
 
-const cylinder6 = cylinder(1, 1, 60, 40)
+const cylinder6 = cylinder(1, 1, 60, 60)
 cylinder6.position.set(-12, 0, 0)
 
-const cylinder7 = cylinder(1, 1, 15, 40)
+const cylinder7 = cylinder(1, 1, 15, 60)
 cylinder7.position.set(42, 7.5, 0)
 
-const cylinder8 = cylinder(1, 1, 15, 40)
+const cylinder8 = cylinder(1, 1, 15, 60)
 cylinder8.position.set(12, 7.5, 0)
 
-// HORIZONTAL CYLINDERS
-const cylinder9 = cylinder(1, 1, 17.5, 40)
-cylinder9.position.set(28, 30, 0)
-cylinder9.rotateZ(90 * (Math.PI / 180))
+const cylinder16 = cylinder(1, 1, 15, 60)
+cylinder16.position.set(-31, 7.5, 0)
 
-const cylinder10 = cylinder(1, 1, 17.5, 40)
+// HORIZONTAL CYLINDERS
+const cylinder9 = cylinder(1, 1, 17.5, 60)
+cylinder9.position.set(28, 30, 0)
+cylinder9.rotateZ(rotation)
+
+const cylinder10 = cylinder(1, 1, 17.5, 60)
 cylinder10.position.set(-4, 30, 0)
-cylinder10.rotateZ(90 * (Math.PI / 180))
+cylinder10.rotateZ(rotation)
+
+const cylinder11 = cylinder(1, 1, 17.5, 60)
+cylinder11.position.set(12, -30, 0)
+cylinder11.rotateZ(rotation)
+
+const cylinder12 = cylinder(1, 1, 30, 60)
+cylinder12.position.set(50.3, -30, 0)
+cylinder12.rotateZ(rotation)
+
+const cylinder13 = cylinder(1, 1, 17.5, 60)
+cylinder13.position.set(56, 30, 0)
+cylinder13.rotateZ(rotation)
+
+const cylinder14 = cylinder(1, 1, 37, 60)
+cylinder14.position.set(-29.8, -30, 0)
+cylinder14.rotateZ(rotation)
+
+const cylinder15 = cylinder(1, 1, 17.5, 60)
+cylinder15.position.set(-39.3, 15, 0)
+cylinder15.rotateZ(rotation)
+
+const cylinder19 = cylinder(1, 1, 24, 60)
+cylinder19.position.set(-39, 27.5, 0)
+cylinder19.rotateZ(rotation)
+
+// DIAGONAL CYLINDERS
+const cylinder17 = cylinder(1, 1, 18.5, 60)
+cylinder17.position.set(-44, 21, 0)
+cylinder17.rotateZ(45 * (Math.PI / 180))
+
+const cylinder18 = cylinder(1, 1, 18.5, 60)
+cylinder18.position.set(-34, 21, 0)
+cylinder18.rotateZ(-45 * (Math.PI / 180))
 
 camera.position.z = 110
 
@@ -89,6 +132,8 @@ group.add(tor1)
 group.add(tor2)
 group.add(tor3)
 group.add(tor4)
+group.add(tor5)
+group.add(tor6)
 
 group.add(cylinder0)
 group.add(cylinder1)
@@ -102,6 +147,15 @@ group.add(cylinder8)
 
 group.add(cylinder9)
 group.add(cylinder10)
+group.add(cylinder11)
+group.add(cylinder12)
+group.add(cylinder13)
+group.add(cylinder14)
+group.add(cylinder15)
+group.add(cylinder16)
+group.add(cylinder17)
+group.add(cylinder18)
+group.add(cylinder19)
 
 scene.add(group)
 
@@ -113,5 +167,6 @@ const animate = () => {
 	renderer.render(scene, camera)
 
 }
+// renderer.render(scene, camera)
 
 renderer.setAnimationLoop(animate)
